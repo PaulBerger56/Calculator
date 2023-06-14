5b87e247b407ff1dd940b502f06e2c9d0c32252e
Paul Berger <paulberger56@gmail.com>
2023-06-13 20:03:24 -0400

    Add a check to equals so it only works with at least 2 numbers./n Add a check to the decimal button to not work if there is one already present

--- Script.js
+++ Script.js
@@ -4,19 +4,22 @@ let operator = '';
 let displayValue = '';
 
 function add(num1, num2) {
-    return parseInt(num1) + parseInt(num2);
+    return (parseInt(num1) + parseInt(num2)).toFixed(2);
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return (num1 - num2).toFixed(2);
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+    return (num1 * num2).toFixed(2);
 }
 
 function divide(num1, num2) {
-    return (num1 / num2);
+    if(num2 === '0') {
+        return 'Nice Try!'
+    } 
+        return (num1 / num2).toFixed(2);   
 }
 
 function operate(num1, num2, operator) {
@@ -41,33 +44,39 @@ function display(val){
         displayValue = displayArea.value + " " + val + " ";
         displayArea.value = displayValue;
     } else {
-        displayValue = displayArea.value  + val;
+        displayValue = displayArea.value + val;
         displayArea.value = displayValue;
-    }
-    
-}
+    }    
+};
 
 function clear() {
     displayArea.value = '';
-}
+};
 
 const displayArea = document.querySelector("#result");
 const buttons = document.querySelectorAll(".regButton");
 const equals = document.querySelector("#equals");
 const clearButton = document.querySelector("#clearButton");
+const decimalButton = document.querySelector("#decimalButton");
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        display(button.value);
-        console.log("click " + button.value);
+        display(button.value);        
     });
 });
 
 equals.addEventListener('click', () => {
     let currentValue = '';
     const values = displayValue.split(" ");
+    
+    if (isNaN(parseInt(values[2]))) {
+        return;
+    }
 
     while(values.length > 1) {
+        console.log(values)
+        console.log('did you get here?')
+        console.log(values.length)
         let num1 = values.shift();
         let operator = values.shift();
         let num2 = values.shift();
@@ -75,12 +84,20 @@ equals.addEventListener('click', () => {
         currentValue = operate(num1, num2, operator);
         values.unshift(currentValue);        
     }   
-    displayArea.value = currentValue;
-    
+    displayArea.value = currentValue;    
 });
 
 clearButton.addEventListener('click', () => {
     displayArea.value = '';
+});
+
+decimalButton.addEventListener('click', () => {
+    if(displayArea.value.includes('.')) {
+        return;
+    }
+    display('.');
+    
+    
 });
 
 
